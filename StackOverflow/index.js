437aa5b4bd6282b7650c503e5b046eacfa7da541
Luiz Henrnique SIlva Jesus <luizhenriquesilvajesus@gmail.com>
2020-05-12 17:00:31 -0300

    metricas do stackoverflow

--- StackOverflow/index.js
+++ StackOverflow/index.js
@@ -3,8 +3,7 @@ const apiStackoverflow = new ApiStackoverflow();
 
 async function start() {
   await apiStackoverflow.startFile();
-  const usersidsSTack = [1780632];
-  // const usersidsSTack = [1780632, 5783700, 355715, 2312051, 2576254];
+  const usersidsSTack = [1780632, 5783700, 355715, 2312051, 2576254];
 
   for (let i = 0; i < usersidsSTack.length; i++) {
     console.log("doind id: " + usersidsSTack[i]);
